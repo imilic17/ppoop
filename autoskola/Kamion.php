359597b1dca66f673cf1628ee119c2d2b59729e5
Franjo Đorđević <franjo.dordevic4@ad.tsd.hr>
2026-02-17 10:44:20 +0100

    Zavrsio sam klasu kamion i sve sto treba u njoj.

---
+++ autoskola/Kamion.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+
+        class Kamion extends Vozilo{
+
+            private $osivost;
+            private $brojOsovina;
+
+
+            public function __construct($marka,$model,$godiste, $cijenaPoSatu, $nosivost, $brojOsovina){
+                parent::__construct($marka, $model, $godiste, $cijenaPoSatu);
+                $this -> nosivost = $nosivost;
+                $this -> brojOsovina = $brojOsovina;
+            }
+    
+            public function getInfo(){
+                return $this -> marka . $this -> model. " (" .$this -> godiste   . ") -" . $this -> cijenaPoSatu. " €/sat ". $this -> nosivost. " t ". $this -> brojOsovina. " osovina." ;
+            }
+
+            public function troskoviPolaganja(){
+                return($this -> cijenaPoSatu * 40) + ($this -> nosivost * 100);
+
+            }
+
+
+    }
+    ?>
+
+</body>
+</html>
