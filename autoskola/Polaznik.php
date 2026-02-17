b4946e80985b482fc79d8940705b80b0cd72335b
Stjepan Patek <stjepan.patek@ad.tsd.hr>
2026-02-17 10:54:54 +0100

    funkcija konstruktora i funkcija za ime

--- autoskola/Polaznik.php
+++ autoskola/Polaznik.php
@@ -6,6 +6,17 @@ class Polaznik
     private $email;
     private $instruktori;
 
+    public function __construct($ime, $email)
+    {
+        $this->ime = $ime;
+        $this->email = $email;
+        $this->instruktori = [];
+    }
+
+    public function getIme()
+    {
+        return $this->ime;
+    }
     
 }
 
