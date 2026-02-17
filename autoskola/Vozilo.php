7399629bcc0d39c58dac243877fc2e925c025d00
Lamia Fridrih <lamia.fridrih@ad.tsd.hr>
2026-02-17 11:18:53 +0100

    uključivanje klase Vozilo

--- autoskola/Vozilo.php
+++ autoskola/Vozilo.php
@@ -19,8 +19,40 @@
             }  else {
                 $this->cijenaposatu = $cijenaposatu;
             }
-      
-        }}
-       
+        }
+
+        public function getInfo()
+        {
+            return "marka: " . $this->marka . ", model: " . $this->model . ", godiste: " . $this->godiste . ", cijena po satu: " . $this->cijenaposatu;
+        }
+
+        public function jeNovo()
+        {
+            if ($godiste>=2024){
+                return $this->godiste;
+            }else{
+                return false;
+            }
+        }
+
+        private $cijena;  
+        public function getCij() { 
+            return $this->cijena; 
+        } 
+        public function setCij($cijena) { 
+            if ($cijena > 0 ) { 
+            $this->dob = $dob; 
+            return true; 
+        } 
+        return false; 
+        } 
+
+            
+
+
+    }
+
+   
+
 
 ?>
