aa3eec146e44150908e3583a636d834571f9f554
Stjepan Patek <stjepan.patek@ad.tsd.hr>
2026-02-17 10:56:21 +0100

    funcikja za email i dodavanje instruktora

--- autoskola/Polaznik.php
+++ autoskola/Polaznik.php
@@ -17,7 +17,26 @@ class Polaznik
     {
         return $this->ime;
     }
-    
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function dodajInstruktora($imeInstruktora, $vozilo)
+    {
+       
+        foreach ($this->instruktori as $instruktor) {
+            if ($instruktor['ime'] === $imeInstruktora) {
+                return;
+            }
+        }
+
+        $this->instruktori[] = [
+            'ime' => $imeInstruktora,
+            'vozilo' => $vozilo
+        ];
+    }
 }
 
 
