dbf930a13416734575e1a8b837413ed1aebd852b
Teo Tuček <teo.tucek@ad.tsd.hr>
2026-02-17 11:07:17 +0100

    dovrsen kamion

---
+++ autoskola/Kamion.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+require_once "Vozilo.php";
+
+class Kamion extends Vozilo {
+    private $nosivost;
+    private $brojOsovina;
+
+    public function __construct($marka, $model, $godiste, $cijenaPoSatu, $nosivost, $brojOsovina){
+        parent::__construct($marka, $model, $godiste, $cijenaPoSatu);
+        $this->nosivost = $nosivost;
+        $this->brojOsovina = $brojOsovina;
+    }
+
+    public function getInfo(){
+        return "$this->marka $this->model ($this->godiste) | {$this->getCijenaPoSatu()} $/sat | {$this->nosivost}t, {$this->brojOsovina} osovina";
+    }
+
+    public function troskoviPolaganja(){
+        return ($this->getCijenaPoSatu() * 40) + ($this->nosivost * 100);
+    }
+}
+?>
+</body>
+</html>
