3e1115057f93e4c3be3303219c0acca9a0e89729
Stjepan Patek <stjepan.patek@ad.tsd.hr>
2026-02-17 10:21:10 +0100

    Dodate su javnew fiunkcije koje su potrebne za ispis

--- autoskola/Vozilo.php
+++ autoskola/Vozilo.php
@@ -8,7 +8,36 @@ class vozilo
 
     private $cijenaPoSatu;
 
-    
+    public function _constructr($marka, $model, $godiste, $cijenaPoSatu)
+    {
+        $this->marka = $marka;
+        $this->model = $model;
+        $this->godiste = $godiste;
 
+        if ($cijenaPoSatu > 0) {
+            $this->cijenaPoSatu = $cijenaPoSatu;
+        } else {
+            $this->cijenaPoSatu = 30;
+        }
+    }
 
+    public function setCijenaPoSatu($cijena)
+    {
+        if ($cijena > 0) {
+            $this->cijenaPoSatu = $cijena;
+        }
+    }
+
+    public function getCijenaPoSatu()
+    {
+        return $this->cijenaPoSatu;
+    }
+
+    public function getInfo() {
+        return "{$this->marka} {$this->model} ({$this->godiste}) | {$this->cijenaPoSatu} €/sat";
+    }
+
+    public function jeNOvo(){
+        return $this->godiste >= 2024;
+    }
 }
