7e61304118e0ea0b3c2d60aed761286619d35591
Teo Tuček <teo.tucek@ad.tsd.hr>
2026-02-17 10:49:37 +0100

    doverseno vozilo

--- autoskola/Vozilo.php
+++ autoskola/Vozilo.php
@@ -6,39 +6,45 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
+<?php
 
-        class Vozilo {
-            public $marka;
-            public $model;
-            public $godiste;
-            private $cijenaPoSatu;
+class Vozilo {
+    public $marka;
+    public $model;
+    public $godiste;
+    private $cijenaPoSatu;
 
-            public function __construct( $marka, $model, $godiste, $cijenaPoSatu){
-                $this->marka = $marka;
-                $this->model = $model;
-                $this->godiste = $godiste;
+    public function __construct($marka, $model, $godiste, $cijenaPoSatu){
+        $this->marka = $marka;
+        $this->model = $model;
+        $this->godiste = $godiste;
 
-
-                if ($cijenaPoSatu <= 0){
-                    $this->cijenaPoSatu = 30;
-                }else{
-                    $this->cijenaPoSatu = $cijena;
-                }
-                }
-            }
-            
+        if ($cijenaPoSatu <= 0){
+            $this->cijenaPoSatu = 30;
+        } else {
+            $this->cijenaPoSatu = $cijena;
         }
+    }
 
+    public function setCijenaPoSatu($cijena){
+        if($cijena > 0 ){
+            $this->setCijenaPoSatu = $cijena;
+        }
+    }
 
+    public function getCijenaPoSatu(){
+        return $this->getCijenaPoSatu;
+    }
 
+    public function getInfo(){
+        return "$this->marka $this->model ($this->godiste) | {$this->getCijenaPoSatu}";
+    }
 
+    public function jeNovo(){
+        return $this->godiste >= 2024;
+    }
+}
 
-
-
-
-
-    ?>
-
+?>
 </body>
 </html>
